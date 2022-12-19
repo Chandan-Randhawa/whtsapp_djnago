78de03f8ef6911d51297b05be95860a391714370
Chandan-Randhawa <zsrandhawa@gmail.com>
2022-12-19 15:39:04 +0530

    deploying herokuu

--- cmcapp/views.py
+++ cmcapp/views.py
@@ -7,117 +7,118 @@ import json
 
 
 def sendwhtsappmessage(request):
-    y = 'EAAPolYdEp4YBAC5mLZAiDM2lMBCsof9ouQKCGRxXgi4cYUevAyqvG06S9lZCaApLWV8hRcpvwD1ZCaz2L62qJ7gmVYSTtxH9bFTjlISIDn2ocJqT4hG3p37x9lZBsxKPZBj9NVe3oLwiP1Bn7BUs9HUCrKF587ktJZCvcif7ev0NSJS1K5zcZC7MZA7sNeIrsSZB9yD84vGD6mxHAPj0BlWxQ'
-    header = {'Authorization': f'{y}'}
-    payload_2 = { "messaging_product": "whatsapp", "to": "919779922228", "type": "template", "template": { "name": "hello_world", "language": { "code": "en_US" } } }
-    pay_3 = {
-            "messaging_product": "whatsapp",
-            "recipient_type": "individual",
-            "to": "919779922228",
-            "type": "interactive",
-            "interactive":{
-                            "type": "list",
-                            "header": {
-                                "type": "text",
-                                "text": "your-header-content"
-                            },
-                            "body": {
-                                "text": "your-text-message-content"
-                            },
-                            "footer": {
-                                "text": "your-footer-content"
-                            },
-                            "action": {
-                                "button": "cta-button-content",
-                                "sections":[
-                                {
-                                    "title":"12",
-                                    "rows": [
-                                    {
-                                        "id":"unique-row-identifier",
-                                        "title": "python",
-                                        "description": "",           
-                                    },
-                                    {
-                                        "id":"unique-row-identifier-2",
-                                        "title": "django ",
-                                        "description": "",           
-                                    }
-                                    ]
-                                },
-                                {
-                                    "title":"34",
-                                    "rows": [
-                                    {
-                                        "id":"unique-row-identifier",
-                                        "title": "software",
-                                        "description": "",           
-                                    }
-                                    ]
-                                },
-                                ]
-                            }
-                            }
-            }
-    payload = {
-                "messaging_product": "whatsapp",
-                "recipient_type": "individual",
-                "to": "919779922228",
-                "type": "template",
-                "template": {
-                    "name": "sample_issue_resolution",
-                    "language": {
-                    "code": "en_US"
-                    },
-                    "components": [
-                    # {
-                    #     "type": "header",
-                    #     "parameters": [
-                    #     {
-                    #         "type": "image",
-                    #         "image": {
-                    #                 "link": "https://i.imgur.com/5NefzrH.png"
+#     x = 'Bearer'
+#     y = 'EAAPolYdEp4YBAC5mLZAiDM2lMBCsof9ouQKCGRxXgi4cYUevAyqvG06S9lZCaApLWV8hRcpvwD1ZCaz2L62qJ7gmVYSTtxH9bFTjlISIDn2ocJqT4hG3p37x9lZBsxKPZBj9NVe3oLwiP1Bn7BUs9HUCrKF587ktJZCvcif7ev0NSJS1K5zcZC7MZA7sNeIrsSZB9yD84vGD6mxHAPj0BlWxQ'
+#     header = {'Authorization': f'{x} {y}'}
+#     payload_2 = { "messaging_product": "whatsapp", "to": "919779922228", "type": "template", "template": { "name": "hello_world", "language": { "code": "en_US" } } }
+#     pay_3 = {
+#             "messaging_product": "whatsapp",
+#             "recipient_type": "individual",
+#             "to": "919779922228",
+#             "type": "interactive",
+#             "interactive":{
+#                             "type": "list",
+#                             "header": {
+#                                 "type": "text",
+#                                 "text": "your-header-content"
+#                             },
+#                             "body": {
+#                                 "text": "your-text-message-content"
+#                             },
+#                             "footer": {
+#                                 "text": "your-footer-content"
+#                             },
+#                             "action": {
+#                                 "button": "cta-button-content",
+#                                 "sections":[
+#                                 {
+#                                     "title":"12",
+#                                     "rows": [
+#                                     {
+#                                         "id":"unique-row-identifier",
+#                                         "title": "python",
+#                                         "description": "",           
+#                                     },
+#                                     {
+#                                         "id":"unique-row-identifier-2",
+#                                         "title": "django ",
+#                                         "description": "",           
+#                                     }
+#                                     ]
+#                                 },
+#                                 {
+#                                     "title":"34",
+#                                     "rows": [
+#                                     {
+#                                         "id":"unique-row-identifier",
+#                                         "title": "software",
+#                                         "description": "",           
+#                                     }
+#                                     ]
+#                                 },
+#                                 ]
+#                             }
+#                             }
+            # }
+    # payload = {
+    #             "messaging_product": "whatsapp",
+    #             "recipient_type": "individual",
+    #             "to": "919779922228",
+    #             "type": "template",
+    #             "template": {
+    #                 "name": "sample_issue_resolution",
+    #                 "language": {
+    #                 "code": "en_US"
+    #                 },
+    #                 "components": [
+    #                 {
+    #                     "type": "header",
+    #                     "parameters": [
+    #                     {
+    #                         "type": "image",
+    #                         "image": {
+    #                                 "link": "https://i.imgur.com/5NefzrH.png"
 
-                    #         }
-                    #     }
-                    #     ]
-                    # },
-                    {
-                        "type": "body",
-                        "parameters": [
-                        {
-                            "type": "text",
-                            "text": "Zoarwar Singh"
-                        },
-                        # {
-                        #     "type": "text",
-                        #     "text": "python"
-                        # },
-                        # {
-                        #     "type": "text",
-                        #     "text": "Postgresql"
-                        # }
-                        ]
-                    },
-                    {
-                        "type": "button",
-                        "sub_type": "quick_reply",
-                        "index": "0",
-                        "parameters": [
-                        {
-                            "type": "payload",
-                            "payload": "PAYLOADdd"
-                        }
-                        ]
-                    }
-                    ]
-                }
-                }
-    print('ssssss')
-    response = requests.post('https://graph.facebook.com/v15.0/106405205659521/messages' , headers= header ,json=pay_3)
-    print('ddddddd')
-    ans = response.json()
-    print(ans)
+    #                         }
+    #                     }
+    #                     ]
+    #                 },
+    #                 {
+    #                     "type": "body",
+    #                     "parameters": [
+    #                     {
+    #                         "type": "text",
+    #                         "text": "Zoarwar Singh"
+    #                     },
+    #                     {
+    #                         "type": "text",
+    #                         "text": "python"
+    #                     },
+    #                     {
+    #                         "type": "text",
+    #                         "text": "Postgresql"
+    #                     }
+    #                     ]
+    #                 },
+    #                 {
+    #                     "type": "button",
+    #                     "sub_type": "quick_reply",
+    #                     "index": "0",
+    #                     "parameters": [
+    #                     {
+    #                         "type": "payload",
+    #                         "payload": "PAYLOADdd"
+    #                     }
+    #                     ]
+    #                 }
+    #                 ]
+    #             }
+    #             }
+    # print('ssssss')
+    # response = requests.post('https://graph.facebook.com/v15.0/106405205659521/messages' , headers= header ,json=pay_3)
+    # print('ddddddd')
+    # ans = response.json()
+    # print(ans)
     return HttpResponse('ifimvmfii')
 
 @csrf_exempt
--- cmcproject/settings.py
+++ cmcproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v=g+d8ud2-bnyk=bv!p+2d*g80xr)kd40ghf1!$+8u7nia=35d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
